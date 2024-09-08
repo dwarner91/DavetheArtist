4b50c664aeb511d47c8485773c943180e416a89e
dwarner91 <142574227+dwarner91@users.noreply.github.com>
2024-09-08 15:25:07 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-$(document.ready(function() {
-$(body).css('background-color','#ff0000');
-  
+$(document).ready(function(){
+$(':header').css('font-size', '35px');
+$('body').css('background-color', 'red');
+$('main').prepend('<img src="dove.jpg" alt="dove wielding a black katana">');
+$('img').css({'width': '750px', 'height': '500px'});
+$('p:last-child').css({'font-size': '20px', 'font-weight': 'bold'});
+$('#enlarge').click(function(){$('img').animate({'width': '1500px', 'height': '1000px'})});
+$('#shrink').click(function(){$('img').animate({'width': '750px', 'height': '500px'})});
 });
